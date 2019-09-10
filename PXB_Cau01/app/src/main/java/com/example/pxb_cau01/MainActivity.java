98879077faecee36e24d1e5273a6ba75ad277856
= <=>
2019-09-10 17:05:44 +0700

    update thoat

--- PXB_Cau01/app/src/main/java/com/example/pxb_cau01/MainActivity.java
+++ PXB_Cau01/app/src/main/java/com/example/pxb_cau01/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.pxb_cau01;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +12,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button bt_dn;
+    Button bt_dn,bt_thoat;
     EditText et_tk1,et_tk2;
     CheckBox ck_luu;
     @Override
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         et_tk2 = (EditText)findViewById(R.id.editText_tk2);
         bt_dn = (Button) findViewById(R.id.button_dangnhap);
         ck_luu = (CheckBox)findViewById(R.id.checkBox_luu);
+        bt_thoat = (Button)findViewById(R.id.button_thoat);
         bt_dn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,6 +39,30 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     Toast.makeText(MainActivity.this, "Vui lòng nhập đủ thông tin", Toast.LENGTH_LONG).show();
                 }
+            }
+        });
+        bt_thoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Bạn có chắc muốn thoát?");
+                builder.setTitle("Thông báo");
+                builder.setIcon(android.R.drawable.stat_sys_warning);
+
+                builder.setPositiveButton("Có", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                        finish();
+                    }
+                });
+                builder.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+                builder.create().show();
             }
         });
     }
